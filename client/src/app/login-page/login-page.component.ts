f7fb1900bb9c1e78386fea2bec20d4a4e5953d2f
Mher <mherf@mail.ru>
2022-09-30 11:08:40 +0400

    create Login and Register page

---
+++ client/src/app/login-page/login-page.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+  public form!: FormGroup;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+
+  public onSubmit() {
+
+  }
+
+}
